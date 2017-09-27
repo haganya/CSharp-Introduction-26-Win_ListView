14e132754b175b46d9777aee88d42ea664df0ca1
Hakan Akgül <hakanakgul34@gmail.com>
2017-09-27 22:38:09 +0300

    created at 11.08.2017 (dd.mm.yyyy)

---
+++ 20170811-1Win_ListView/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace _20170811_1Win_ListView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtFilmAdi = new System.Windows.Forms.TextBox();
+            this.txtTur = new System.Windows.Forms.TextBox();
+            this.txtYil = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 76);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(410, 272);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Film Adı";
+            this.columnHeader1.Width = 139;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Tür";
+            this.columnHeader2.Width = 158;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Yıl";
+            this.columnHeader3.Width = 105;
+            // 
+            // txtFilmAdi
+            // 
+            this.txtFilmAdi.Location = new System.Drawing.Point(12, 23);
+            this.txtFilmAdi.Name = "txtFilmAdi";
+            this.txtFilmAdi.Size = new System.Drawing.Size(144, 20);
+            this.txtFilmAdi.TabIndex = 0;
+            // 
+            // txtTur
+            // 
+            this.txtTur.Location = new System.Drawing.Point(162, 23);
+            this.txtTur.Name = "txtTur";
+            this.txtTur.Size = new System.Drawing.Size(143, 20);
+            this.txtTur.TabIndex = 1;
+            // 
+            // txtYil
+            // 
+            this.txtYil.Location = new System.Drawing.Point(311, 23);
+            this.txtYil.Name = "txtYil";
+            this.txtYil.Size = new System.Drawing.Size(111, 20);
+            this.txtYil.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Film Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(215, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tür";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(355, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Yıl";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(347, 47);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 3;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnEkle;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 360);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtYil);
+            this.Controls.Add(this.txtTur);
+            this.Controls.Add(this.txtFilmAdi);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.TextBox txtFilmAdi;
+        private System.Windows.Forms.TextBox txtTur;
+        private System.Windows.Forms.TextBox txtYil;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnEkle;
+    }
+}
+
---
+++ 20170811-1Win_ListView/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20170811_1Win_ListView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         * ListView nesnesine kolonlar eklemen için properties üzerinden Colums seçilerek ekleme yapılır.
+         * Kolonların görünülür yapılabilmesi için listview propertiesinde view özelliği details olarak değiştirilmelidir.
+         * Eklenen kolonların üzerinde yazılan yazıların değiştirilmesi için Colums eklemesinin yapıldığı yerde Text değeri değiştirilir. NAME DEĞİL TEXT!
+         * ListView nesnesine değer ataman için öncesinde bir ListViewItem nesnesi doldurulmalıdır. sonrasında bu nesne listviewe gönderilir ve ekleme işlemi yapılmış olur.
+         * ListViewItem nesnesinin Text özelliği ilk columna denk gelir.
+         * Diğer columnlar subitem olarak alınır.
+         * Listview üzerinde ızgaraların görünmesini istiyorsak properties penceresinden GridLines özelliğini TRUE yapmamız gerekir. 
+         * Bütün satırı seçmek isteniyorsa FullRowSelect özelliği true yapılmalıdır.
+         */
+         
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            ListViewItem satir = new ListViewItem();
+            satir.Text = txtFilmAdi.Text; // ilk column
+            satir.SubItems.Add(txtTur.Text); //2. column
+            satir.SubItems.Add(txtYil.Text); //3. column
+
+            listView1.Items.Add(satir); //ListViewItem nesnesini al listviewe ekle
+
+            txtFilmAdi.Clear();
+            txtTur.Clear();
+            txtYil.Clear();
+            txtFilmAdi.Focus();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
